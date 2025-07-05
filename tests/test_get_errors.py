a2aee99dd8601aefee5678b24b687428325ed320
KostantinRudenko <rudenkokostya20@gmail.com>
2025-07-05 10:11:06 +0300

    modified test_get_errors_2 and added new static method

--- tests/test_get_errors.py
+++ tests/test_get_errors.py
@@ -4,14 +4,14 @@ from .testtools import TestTools
 from modules.get_pid import get_errors, ValueOutOfRangeError
 
 
-@pytest.mark.parametrize("arr, expected", TestTools.get_random_values_for_get_errors())
+@pytest.mark.parametrize("arr, expected", TestTools.get_random_values())
 def test_get_errors_1(arr, expected):
     """
     Cheching if input values are equal to the expected ones
     """
     assert get_errors(arr) == expected
 
-@pytest.mark.parametrize("arr", [([8000]), ([-1]), ([-1, 8000])])
+@pytest.mark.parametrize("arr", TestTools.get_wrong_values())
 def test_get_errors_2(arr):
     """
     Checking if function reacts on wrong values
--- tests/testtools.py
+++ tests/testtools.py
@@ -2,7 +2,7 @@ from random import randint
 
 class TestTools:
     @staticmethod
-    def get_random_values_for_get_errors():
+    def get_random_values():
         values = []
         for i in range(0, 10):
             original_vals = []
@@ -14,4 +14,12 @@ class TestTools:
             values.append((original_vals, error_vals))
         return values
 
-print(TestTools.get_random_values_for_get_errors())
+    @staticmethod
+    def get_wrong_values():
+        values = []
+        for i in range(0, 5):
+            values.append((list([randint(-1000, -1) for i in range(0, 10)])))
+        for i in range(0, 5):
+            values.append((list([randint(7001, 8000) for i in range(0, 10)])))
+
+        return values
