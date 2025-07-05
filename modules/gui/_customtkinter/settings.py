9a60fb4ac9c0f9b864a9f522caae6cdf4cdb2c8a
KostantinRudenko <rudenkokostya20@gmail.com>
2025-07-05 13:20:14 +0300

    added png icon and made it to be displayed as a window icon

--- modules/gui/_customtkinter/settings.py
+++ modules/gui/_customtkinter/settings.py
@@ -1,6 +1,7 @@
 import os
 
 import customtkinter as ctk
+from tkinter import PhotoImage
 from ..palette import color_palette
 
 class Window(ctk.CTk):
@@ -9,17 +10,15 @@ class Window(ctk.CTk):
             self,
             fg_color=color_palette["primary"]
         )
-
-        self.title("e-formula coefficient visualizer")
-        self.iconbitmap(
-            os.path.abspath(
-                path=os.path.join(
-                    __file__,
-                    "..", "..", "..", "..",
-                    "static", "icons", "icon.ico"
+        self.iconphoto(True, PhotoImage(
+            file=os.path.abspath(path=os.path.join(
+                __file__, "..", "..", "..", "..",
+                "static", "icons", "icon.png"
+                    )
                 )
             )
         )
+        self.title("e-formula coefficient visualizer")
         self.geometry("960x540")
 
 window = Window()
